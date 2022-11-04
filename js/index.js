0d2b09379e5e45d0332dfe97f525fec6255a3ac9
Alaa Aldandachi <58466561+AlaaDan@users.noreply.github.com>
2022-11-04 15:17:44 +0100

    Code updated

--- js/index.js
+++ js/index.js
@@ -1,109 +1,91 @@
-/**
- För att toggla SVG:en
- document.querySelector('figure').classList.add('scaffold')
- document.querySelector('figure').classList.add('head')
- document.querySelector('figure').classList.add('body')
- document.querySelector('figure').classList.add('arms')
- document.querySelector('figure').classList.add('legs')
-
- */
-
-let hangMan =['scaffold','head','body', 'arms', 'legs' ];
-//let wordList = ["apple", "camel", "sweden"];
-let randomWord = wordList[Math.floor(Math.random()*wordList.length)];
+let hangMan = ['scaffold', 'head', 'body', 'arms', 'legs'];
+let randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+console.log(randomWord);
 let display = Array(randomWord.length).fill("_");
 let lives = 0;
 let wrongLetter = [];
-//let userGuess = '';
-let displayHTML = document.querySelector("#display");
-displayHTML.innerHTML = display.join(" ");
-let inputHTML = document.querySelector('#user-guess');
-let wrongLetterHTML = document.querySelector("#wrong-letters");
-let playButton = document.querySelector("#play");
 let interval;
-
-playButton.addEventListener('click', () => {
+// get HTML elements:
+let playButton = document.querySelector("#play");
+let inputHTML = document.querySelector('#user-guess');
+let displayHTML = document.querySelector("#display");
+let wrongLetterHTML = document.querySelector("#wrong-letters");
+let gameoverHTML = document.querySelector("#gameover");
+let playAgainButton = document.querySelector("#play-again");
+displayHTML.innerHTML = display.join(" ");
+// add eventlisteners:
+playButton.addEventListener('click', function playListener() {
     playButton.style.display = "none";
-    document.querySelector('.game-container').style.display ="block";
+    document.querySelector('.game-container').style.display = "block";
+    inputHTML.focus();
     let count = 60;
-    interval = setInterval(function(){
-    document.getElementById('count').innerHTML=count;
-    if (count === 0){
-        gameover('timeout');
-    }
-    count--;
+    interval = setInterval(function () {
+        document.getElementById('count').innerHTML = count;
+        if (count === 0) {
+            gameover('timeout');
+        }
+        count--;
     }, 1000);
 })
 
-function getKeyboardKey(){
-    inputHTML.addEventListener('keydown', (event) => {
-        if(event.key === "Enter") {
-            letterChecker(inputHTML.value);
-        }
-    })
-}
-console.log(randomWord);
-
-function letterChecker(character){
-
-    inputHTML.value = "";
-    let userGuess = character.toLowerCase();
-    console.log(userGuess);
-    if(userGuess.length === 1){
-        for (let i =0; i < randomWord.length; i++){
-            let letter = randomWord[i]
-            if(userGuess === letter){
-                display.splice(i, 1, letter);
-            }
-        }
-
-    } else {
-        alert("Enter 1 letter at a time");
-        return
-    }
-    if(randomWord.includes(userGuess) === false){
-        if(!wrongLetter.includes(userGuess)){
-            wrongLetter.push(userGuess);
-            document.querySelector('figure').classList.add(hangMan[lives]);
-            lives ++;
-        } else {
-            // alert("Already guessed that letter")
-        }
-        
-
-        if(lives === 5) {
-            gameover('loss')
-        }
-    }
-    else if(display.includes("_") === false){
-        gameover("win");
-    }
+playAgainButton.addEventListener('click', () => {
+    //nollställl spelet:
+    inputHTML.style.display = "inline";
+    playAgainButton.style.display = 'none'
+    wrongLetterHTML.innerHTML = "";
+    randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+    console.log(randomWord);
+    display = Array(randomWord.length).fill("_");
     displayHTML.innerHTML = display.join(" ");
-    wrongLetterHTML.innerHTML = wrongLetter.join(" ");
-}
+    lives = 0;
+    wrongLetter = [];
+    gameoverHTML.innerHTML = "";
+    playButton.click(); // simulera ett klick på play för att starta nytt spel
+})
 
-getKeyboardKey();
+inputHTML.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
+        let character = inputHTML.value;
+        inputHTML.value = "";
+        let userGuess = character.toLowerCase();
+        if (userGuess.length === 1) {
+            for (let i = 0; i < randomWord.length; i++) {
+                let letter = randomWord[i]
+                if (userGuess === letter) {
+                    display.splice(i, 1, letter);
+                }
+            }
+        } else {
+            alert("Enter 1 letter at a time");
+            return
+        }
+        if (randomWord.includes(userGuess) === false) {
+            if (!wrongLetter.includes(userGuess)) {
+                wrongLetter.push(userGuess);
+                document.querySelector('figure').classList.add(hangMan[lives]);
+                lives++;
+            }
+            if (lives === 5) {
+                gameover('loss')
+            }
+        } else if (display.includes("_") === false) {
+            gameover("win");
+        }
+        displayHTML.innerHTML = display.join(" ");
+        wrongLetterHTML.innerHTML = wrongLetter.join(" ");
+    }
+})
 
 function gameover(text) {
     clearInterval(interval);
-    let gameoverHTML = document.querySelector("#gameover");
-    let playAgainButton = document.querySelector("#play-again");
     inputHTML.style.display = "none";
     playAgainButton.style.display = 'block'
-    playAgainButton.addEventListener('click', () => {
-        location.reload();
-    })
-    if(text === 'loss'){
+    if (text === 'loss') {
         gameoverHTML.innerHTML = `Game Over, you lost... The word was: ${randomWord}`;
-        // inputHTML.style.display = "none";
-        // playButton.style.display = "block"
-    }
-    else if (text === "win"){
+    } else if (text === "win") {
         gameoverHTML.innerHTML = "You guessed the word, Congratz!";
-        // inputHTML.style.display = "none";
     } else {
         gameoverHTML.innerHTML = `Game over! You ran out of time... The word was: ${randomWord}`;
 
     }
 }
-
